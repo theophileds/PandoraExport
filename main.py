5468e15fbb12a2280b4e3559af0ed5172bc90bf4
Theophile Dunoyer <theophileds@gmail.com>
2024-05-16 17:37:31 -0400

    Add artist and song selection menu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """PandoraExporter
 
 Usage:
-  main.py [--output result.json] [--limit 100] [--artist Echo_Delta]
+  main.py [--output result.json] [--limit 100] [--artist Echo_Delta] [--song None]
   main.py [-h | --help]
 
 Options:
@@ -9,11 +9,13 @@ Options:
   --output=filename  Output file name [default: output.json]
   --limit=100        Maximum number of songs [default: 100]
   --artist=name      Name of the artist with _ instead of space [default: Echo_Delta]
+  --song=song        Name of the song with _ instead of space [default: None]
 
 """
 
 import json
 import logging
+from pick import pick
 from collections import defaultdict
 
 from docopt import docopt
@@ -26,36 +28,50 @@ logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.INFO)
 
 class PandoraExporter(APIClientBuilder):
     def __init__(self, settings, **kwargs):
-        self.settings = settings
         super().__init__(**kwargs)
+        self.settings = settings
+        self.client = self.build_from_settings_dict(settings)
+    
+    def login(self):
+        self.client.login(self.settings['USERNAME'], self.settings['PASSWORD'])
+    
+    def search_song(self, artist, song):
+        search = self.client.search(artist + ' ' + song)
+        if search.songs:
+            option, index = pick([song.song_name for song in search.songs], 'Select the song from {} you want to generate the playlist for:'.format(artist))
+            return search.songs[index].token
+        else:
+            logging.warning('No Songs found for: {}'.format(artist))
 
-    def _build_client(self):
-        return self.build_from_settings_dict(self.settings)
+    def search_artist(self, artist):
+        search = self.client.search(artist)
+        print(search.artists)
+        if search.artists:
+            option, index = pick([artist.artist for artist in search.artists], 'Select the artist you want to generate the playlist for:')
+            return search.artists[index].token
+        else:
+            logging.warning('No artist found for: {}'.format(artist))
 
-    def generate_playlist(self, artist, limit):
-        client = self._build_client()
-        client.login(self.settings['USERNAME'], self.settings['PASSWORD'])
-
-        search_result = client.search(artist)
-        if search_result.artists:
-            songs_batch = defaultdict(list)
+    def generate_playlist(self, token, limit):
+        if token:
+            playlist = defaultdict(list)
             # Each API call retrieve 4 songs
             for i in range(limit // 4):
-                station_result = client.create_station(search_result.artists[0].token)
+                station = self.client.create_station(token)
                 try:
-                    playlist_result = client.get_playlist(station_result.token)
+                    songs = self.client.get_playlist(station.token)
                 except PandoraException as e:
                     logging.info('Pandora exception error: {}'.format(e))
                     break
-                for song in list(filter(lambda track: not track.is_ad, playlist_result)):
-                    if song.song_name not in songs_batch[song.artist_name]:
-                        songs_batch[song.artist_name].append(song.song_name)
+                for song in list(filter(lambda track: not track.is_ad, songs)):
+                    if song.song_name not in playlist[song.artist_name]:
+                        playlist[song.artist_name].append(song.song_name)
                     logging.info('Fetching new song: {} - {}'.format(song.artist_name, song.song_name))
 
-                client.delete_station(station_result.token)
-            return songs_batch
+                self.client.delete_station(station.token)
+            return playlist
         else:
-            logging.warning('No artist found for: {}'.format(artist))
+            logging.warning('No token provided')
 
     @classmethod
     def export_playlist(cls, playlist, output):
@@ -65,10 +81,14 @@ class PandoraExporter(APIClientBuilder):
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    artist_name = ' '.join(arguments.get('--artist').split('_'))
-    songs_limit = int(arguments.get('--limit'))
+    artist = ' '.join(arguments.get('--artist').split('_'))
+    song = ' '.join(arguments.get('--song').split('_'))
+    limit = int(arguments.get('--limit'))
     output_filename = arguments.get('--output')
+    token = ''
 
     pandora_client = PandoraExporter(pandora_credentials)
-    pandora_playlist = pandora_client.generate_playlist(artist_name, songs_limit)
-    pandora_client.export_playlist(pandora_playlist, output_filename)
+    pandora_client.login()
+    token = pandora_client.search_song(artist, song) if song != 'None' else pandora_client.search_artist(artist)
+    playlist = pandora_client.generate_playlist(token, limit)
+    pandora_client.export_playlist(playlist, output_filename)
